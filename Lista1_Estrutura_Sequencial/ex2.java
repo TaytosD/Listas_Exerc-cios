709fa7a4104c0ec7a77d0e671a8086d2470ed761
Davi Samuel <128612321+TaytosD@users.noreply.github.com>
2023-06-23 15:08:20 -0300

    Create ex2.java

---
+++ Lista1_Estrutura_Sequencial/ex2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    Scanner entrada = new Scanner(System.in);
+    System.out.println("digite um numero");
+    int numero;
+    numero = entrada.nextInt();
+    System.out.println("Seu numero é: " + numero);
+    entrada.close();
+  }
+}
