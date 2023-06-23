6afcf56bfff88858f278c60d7196349c224226cb
Davi Samuel <128612321+TaytosD@users.noreply.github.com>
2023-06-23 19:16:14 -0300

    Create ex01.java

---
+++ Lista8_Classes/ex01.java
@@ -0,0 +1,12 @@
+public class Bola {
+  public String cor;
+  public String material;
+  public double circunferencia;
+
+  public void trocaCor(String novaCor){
+    cor = novaCor
+  }
+  public void mostrarCor() {
+    System.out.println(cor);=
+  }
+}
