21271e9cba66bf643a0d8106adadbbba69e695ba
Davi Samuel <128612321+TaytosD@users.noreply.github.com>
2023-06-23 23:42:33 -0300

    Create ex12.java

---
+++ Lista8_Classes/ex12.java
@@ -0,0 +1,17 @@
+public class ContaInvestimento {
+  private double saldo;
+  private double taxaJuros;
+
+  public ContaInvestimento(double saldoInicial, double taxaJuros) {
+        this.saldo = saldoInicial;
+        this.taxaJuros = taxaJuros;
+    }
+  public void adicioneJuros() {
+    double juros = saldo * taxaJuros / 100;
+    saldo += juros;
+  }
+
+  public double verSaldo(){
+    return saldo;
+  }
+}
